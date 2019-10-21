c4fdd38a97a941f66137d39bfec97cf8cf215010
Stuart Herbert <stuart@stuartherbert.com>
2019-10-21 15:38:17 +0100

    New: added type for when we need ability to set a variable via parameter expansion

--- aliases.go
+++ aliases.go
@@ -35,6 +35,10 @@
 
 package shellexpand
 
+// AssignVar sets a key to a given value. If it cannot do so, it reports
+// an error to explain why
+type AssignVar func(string, string) error
+
 // LookupVar is a mapping function. Given a key, it returns either:
 //
 // (matching value, true), or
